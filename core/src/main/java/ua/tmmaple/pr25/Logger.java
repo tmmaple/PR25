0c6ceb990fcb667a1fd4dc9760ed2511a3555c86
afiliushkin <cenyafilyushkin@gmail.com>
2025-05-31 11:16:38 +0300

    Logger: документація

--- core/src/main/java/ua/tmmaple/pr25/Logger.java
+++ core/src/main/java/ua/tmmaple/pr25/Logger.java
@@ -2,39 +2,79 @@ package ua.tmmaple.pr25;
 
 import com.badlogic.gdx.Gdx;
 
+/**
+ * Глобальний логгер для гри.
+ * @author uwuhasmile
+ */
 public final class Logger {
+    /**
+     * Виводить інформацію
+     * @author uwuhasmile
+     */
     public static void info(String msg) {
         Gdx.app.log("INFO", msg);
     }
 
+    /**
+     * Виводить інформацію з тегом класу
+     * @author uwuhasmile
+     */
     public static void info(Class<?> cl, String msg) {
         Gdx.app.log("INFO " + cl.getSimpleName(), msg);
     }
 
+    /**
+     * Виводить інформацію з власним тегом
+     * @author uwuhasmile
+     */
     public static void info(String tag, String msg) {
         Gdx.app.log("INFO " + tag, msg);
     }
 
+    /**
+     * Виводить помилку
+     * @author uwuhasmile
+     */
     public static void error(String msg) {
         Gdx.app.error("ERROR", msg);
     }
 
+    /**
+     * Виводить помилку з тегом класу
+     * @author uwuhasmile
+     */
     public static void error(Class<?> cl, String msg) {
         Gdx.app.error("ERROR " + cl.getSimpleName(), msg);
     }
 
+    /**
+     * Виводить помилку з власним тегом
+     * @author uwuhasmile
+     */
     public static void error(String tag, String msg) {
         Gdx.app.error("ERROR " + tag, msg);
     }
 
+    /**
+     * Виводить повідомлення для дебагу
+     * @author uwuhasmile
+     */
     public static void debug(String msg) {
         Gdx.app.debug("DEBUG", msg);
     }
 
+    /**
+     * Виводить повідомлення для дебагу з тегом класу
+     * @author uwuhasmile
+     */
     public static void debug(Class<?> cl, String msg) {
         Gdx.app.debug("DEBUG " + cl.getSimpleName(), msg);
     }
 
+    /**
+     * Виводить повідомлення для дебагу з власним тегом
+     * @author uwuhasmile
+     */
     public static void debug(String tag, String msg) {
         Gdx.app.debug("DEBUG " + tag, msg);
     }
