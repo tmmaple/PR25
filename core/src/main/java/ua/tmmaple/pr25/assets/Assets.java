b78adb13a7a8e38543bf3db67a9191aa41193b00
wwhasmile <uwuhasmile@proton.me>
2025-05-31 17:28:07 +0300

    Assets: документація

--- core/src/main/java/ua/tmmaple/pr25/assets/Assets.java
+++ core/src/main/java/ua/tmmaple/pr25/assets/Assets.java
@@ -11,6 +11,10 @@ import ua.tmmaple.pr25.audio.Bgm;
 import ua.tmmaple.pr25.graphics.Anm;
 import ua.tmmaple.pr25.util.PR25RuntimeException;
 
+/**
+ * Керує завантаженням та вивантаженням ресурсів.
+ * @author uwuhasmile
+ */
 public final class Assets {
     public static Assets global;
 
@@ -22,45 +26,86 @@ public final class Assets {
         loaded = false;
     }
 
+    /**
+     * Реєструє global в список оновлення.
+     * @author uwuhasmile
+     */
     public static int register() {
         Flow.FlowNode<Assets> node = new Flow.FlowNode<>(global, Assets::update, Assets::added, Assets::removed);
         return Flow.global.addToUpdate(node, 999);
     }
 
+    /**
+     * @return ресурс типу T за іменем
+     * @throws PR25RuntimeException якщо ресурс ще не був завантажений
+     */
     public <T> T get(Class<T> type, String filename) {
         if (!manager.isLoaded(filename)) throw new PR25RuntimeException("Asset " + filename + " is not loaded");
         return manager.get(filename, type);
     }
 
+    /**
+     * Додає ресурс до черги завантажень, якщо його ще не завантажено.
+     * @author uwuhasmile
+     */
     public <T> void load(Class<T> type, String filename) {
         manager.load(filename, type);
     }
 
+    /**
+     * Додає ресурс до черги завантажень, якщо його ще не завантажено.
+     * @param params Параметри для завантаження ресурсу.
+     * @author uwuhasmile
+     */
     public <T> void load(Class<T> type, String filename, AssetLoaderParameters<T> params) {
         manager.load(filename, type, params);
     }
 
+    /**
+     * Вивантажує ресурс, якщо ним ніхто не користується.
+     * @author uwuhasmile
+     */
     public void unload(String filename) {
         manager.unload(filename);
     }
 
+    /**
+     * Вивантажує всі ресурси, якщо вони не використовуються.
+     * @author uwuhasmile
+     */
     public void unload() {
         for (String a : manager.getAssetNames())
             manager.unload(a);
     }
 
+    /**
+     * Повністю очищує всі ресурси.
+     * @author uwuhasmile
+     */
     public void flush() {
         manager.clear();
     }
 
+    /**
+     * @return чи не завантажуються ніякі ресурси
+     * @author uwuhasmile
+     */
     public boolean isLoaded() {
         return loaded;
     }
 
+    /**
+     * @return чи завантажений ресурс filename
+     * @author uwuhasmile
+     */
     public boolean isLoaded(String filename) {
         return manager.isLoaded(filename);
     }
 
+    /**
+     * Ініціалізує екземпляр Assets після додавання до списку оновлення.
+     * @author uwuhasmile
+     */
     private static int added(Assets assets) {
         assets.manager = new AssetManager();
         assets.manager.setLoader(Anm.class, new AnmLoader(new InternalFileHandleResolver()));
@@ -68,6 +113,11 @@ public final class Assets {
         return 0;
     }
 
+    /**
+     * Оновлює екземпляр Assets.
+     * Під час одного оновлення намагається завантажити якомога більше ресурсів.
+     * @author uwuhasmile
+     */
     private static int update(Assets instance) {
         instance.loaded = instance.manager.update(17);
         if (!instance.loaded) {
@@ -76,6 +126,10 @@ public final class Assets {
         return Flow.FLOW_RESULT_CONTINUE;
     }
 
+    /**
+     * Очищається від всіх ресурсів після видалення зі списку оновлення.
+     * @author uwuhasmile
+     */
     private static int removed(Assets instance) {
         instance.manager.dispose();
         Assets.global = null;
