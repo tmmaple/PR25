57b59ed3524f72734ff5dd53827d2fe2c3261388
afiliushkin <cenyafilyushkin@gmail.com>
2025-06-11 09:13:31 +0300

    Enemy: неймінг

--- core/src/main/java/ua/tmmaple/pr25/stages/StageTest.java
+++ core/src/main/java/ua/tmmaple/pr25/stages/StageTest.java
@@ -2,7 +2,6 @@ package ua.tmmaple.pr25.stages;
 
 import com.badlogic.gdx.math.MathUtils;
 import ua.tmmaple.pr25.assets.Stage;
-import ua.tmmaple.pr25.entities.Enemy;
 import ua.tmmaple.pr25.task.Task;
 import ua.tmmaple.pr25.task.TimelineTask;
 import ua.tmmaple.pr25.util.Tweener;
@@ -26,14 +25,14 @@ public class StageTest extends Stage {
             Task.keyframe(
                 en -> {
                     en.changeAngle(Tweener.INTERPOLATION_LINEAR, -MathUtils.HALF_PI, (short) 0);
-                    en.changeVelocity(Tweener.INTERPOLATION_LINEAR, 2.0f, (short) 0);
+                    en.changeSpeed(Tweener.INTERPOLATION_LINEAR, 2.0f, (short) 0);
                     return true;
                 }
             ),
             Task.keyframe(
                 (short) 60,
                 en -> {
-                    en.setLinearMove(Tweener.INTERPOLATION_LINEAR, 0.0f, 4.0f, (short) 50);
+                    en.changeVelocity(Tweener.INTERPOLATION_LINEAR, 0.0f, 4.0f, (short) 50);
                     return true;
                 }
             ),
