5f313bf6926d15053140006c337d597f72b18ba2
wwhasmile <uwuhasmile@proton.me>
2025-06-14 13:24:18 +0300

    Background: документація

--- core/src/main/java/ua/tmmaple/pr25/entities/Background.java
+++ core/src/main/java/ua/tmmaple/pr25/entities/Background.java
@@ -7,6 +7,10 @@ import ua.tmmaple.pr25.graphics.Anm;
 import ua.tmmaple.pr25.graphics.GraphicManager;
 import ua.tmmaple.pr25.util.Tweener;
 
+/**
+ * Фон під час ігрового процесу
+ * @author uwuhasmile
+ */
 public final class Background {
     public static Background global;
 
@@ -29,6 +33,10 @@ public final class Background {
 
     private final GraphicManager.AnmVirtualMachine vm;
 
+    /**
+     * Реєструє в списки оновлення та відмалювання.
+     * @author uwuhasmile
+     */
     public static int register() {
         updateNode = new Flow.FlowNode<>(global, Background::update);
         drawNode = new Flow.FlowNode<>(global, Background::draw);
@@ -37,6 +45,10 @@ public final class Background {
         return 0;
     }
 
+    /**
+     * Видаляє зі списків оновлення та відмалювання.
+     * @author uwuhasmile
+     */
     public static void shutdown() {
         Flow.global.cut(updateNode);
         Flow.global.cut(drawNode);
@@ -51,6 +63,97 @@ public final class Background {
         cameraShakeOffset = new Vector2();
     }
 
+
+    /**
+     * Трясе камеру.
+     * @param power сила трясіння. Накопичується.
+     * @author uwuhasmile
+     */
+    public void shakeCamera(int power) {
+        cameraShakePower += power;
+    }
+
+    /**
+     * Встановлює позицію камери.
+     * @author uwuhasmile
+     */
+    public void setCameraPosition(float cameraPosition) {
+        this.cameraPosition = cameraPosition;
+        vm.teleport();
+    }
+
+    /**
+     * Рухає камеру з поточної до іншої точки протягом певного проміжку часу.
+     * @param ticks проміжок часу, за який камера дійде до точки
+     * @param to точка, до якої має рухатись камера
+     * @param interpolation тип згладжування, з яким рухається камера:
+     *                      <ul>
+     *                      <li><code>INTERPOLATION_LINEAR</code> - лінійний рух</li>
+     *                      <li><code>INTERPOLATION_EASE_IN</code> - починає повільно, закінчує швидко</li>
+     *                      <li><code>INTERPOLATION_EASE_OUT</code> - починає швидко, закінчує повільно</li>
+     *                      <li><code>INTERPOLATION_EASE_IN_OUT</code> - починає повільно, прискорюється, потім сповільнюється</li>
+     *                      </ul>
+     * @author uwuhasmile
+     */
+    public void moveCamera(short ticks, float to, byte interpolation) {
+        positionTweener.start(interpolation, cameraPosition, to, ticks);
+    }
+
+    /**
+     * Змінює швидкість камери за певний проміжок часу.
+     * @param ticks проміжок часу, за який камера має змінити швидкість
+     * @param to швидкість, якої має досягти камера
+     * @param interpolation тип згладжування, з яким змінюється швидкість:
+     *                      <ul>
+     *                      <li><code>INTERPOLATION_LINEAR</code> - лінійний рух</li>
+     *                      <li><code>INTERPOLATION_EASE_IN</code> - починає повільно, закінчує швидко</li>
+     *                      <li><code>INTERPOLATION_EASE_OUT</code> - починає швидко, закінчує повільно</li>
+     *                      <li><code>INTERPOLATION_EASE_IN_OUT</code> - починає повільно, прискорюється, потім сповільнюється</li>
+     *                      </ul>
+     * @author uwuhasmile
+     */
+    public void accelerateCamera(short ticks, float to, byte interpolation) {
+        velocityTweener.start(interpolation, cameraVelocity, to, ticks);
+    }
+
+    /**
+     * Встановлює межі, в яких камера може рухатись.
+     * @param min найнижча точка
+     * @param max найвища точка
+     * @author uwuhasmile
+     */
+    public void setCameraLimits(float min, float max) {
+        setCameraLimits(min, max, false);
+    }
+
+    /**
+     * Встановлює межі, в яких камера може рухатись.
+     * @param min найнижча точка
+     * @param max найвища точка
+     * @param loop визначає, чи має камера телепортуватись до протилежної межі до досягненої
+     * @author uwuhasmile
+     */
+    public void setCameraLimits(float min, float max, boolean loop) {
+        cameraMinPos = min;
+        cameraMaxPos = max;
+        this.loop = loop;
+    }
+
+    /**
+     * Скидає межі камери.
+     * @author uwuhasmile
+     */
+    public void resetCameraLimits() {
+        cameraMinPos = 0.0f;
+        cameraMaxPos = Float.MAX_VALUE;
+        loop = false;
+    }
+
+    /**
+     * Завантажує зображення фону.
+     * @param anm ANM-ресурс з анімаціями та зображенням
+     * @param scriptName скрипт анімації
+     */
     public void load(Anm anm, String scriptName) {
         vm.loadAnm(anm);
         vm.loadScriptAndPlay(scriptName);
@@ -60,12 +163,21 @@ public final class Background {
         cameraShakeOffset.set(0.0f, 0.0f);
     }
 
+    /**
+     * Вивантажує поточний фон та зупиняє всі твінери.
+     * @author uwuhasmile
+     */
     public void unload() {
         positionTweener.end();
         velocityTweener.end();
+        cameraShakePower = 0;
         vm.delete();
     }
 
+    /**
+     * Оновлює фон, рухаючи його та оновлюючи трясіння камери, інтерполяції, і т.д.
+     * @author uwuhasmile
+     */
     private int update() {
         if (!GameplayManager.global.canUpdate() || Player.global.isDeathBombing())
             return Flow.FLOW_RESULT_CONTINUE;
@@ -97,39 +209,10 @@ public final class Background {
         return Flow.FLOW_RESULT_CONTINUE;
     }
 
-    public void shakeCamera(int power) {
-        cameraShakePower += power;
-    }
-
-    public void setCameraPosition(float cameraPosition) {
-        this.cameraPosition = cameraPosition;
-        vm.teleport();
-    }
-
-    public void moveCamera(short ticks, float to, byte interpolation) {
-        positionTweener.start(interpolation, cameraPosition, to, ticks);
-    }
-
-    public void accelerateCamera(short ticks, float to, byte interpolation) {
-        velocityTweener.start(interpolation, cameraVelocity, to, ticks);
-    }
-
-    public void setCameraLimits(float min, float max) {
-        setCameraLimits(min, max, false);
-    }
-
-    public void setCameraLimits(float min, float max, boolean loop) {
-        cameraMinPos = min;
-        cameraMaxPos = max;
-        this.loop = loop;
-    }
-
-    public void resetCameraLimits() {
-        cameraMinPos = 0.0f;
-        cameraMaxPos = Float.MAX_VALUE;
-        loop = false;
-    }
-
+    /**
+     * Відмальовує фон на екрані.
+     * @author uwuhasmile
+     */
     private int draw() {
         vm.draw();
         return Flow.FLOW_RESULT_CONTINUE;
